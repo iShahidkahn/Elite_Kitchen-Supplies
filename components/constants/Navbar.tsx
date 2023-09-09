2eea4a9a02128b7d9d8dfc3f1a6b0104ab4e5f6e
Shahid khan <l3tsc0de@gmail.com>
2023-09-10 04:41:43 +0500

    3rd

--- components/constants/Navbar.tsx
+++ components/constants/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
     <Disclosure as="nav" className="bg-white nav">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl md:px-2 px-0 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
