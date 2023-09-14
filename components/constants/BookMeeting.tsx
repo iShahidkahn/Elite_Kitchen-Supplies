643f3566e33de12d5fc34f694a08e2081fbb3e21
Shahid khan <l3tsc0de@gmail.com>
2023-09-14 20:55:22 +0500

    234

--- components/constants/BookMeeting.tsx
+++ components/constants/BookMeeting.tsx
@@ -5,10 +5,10 @@ const BookUS = () => {
   return (
 
     <div className="book-meeting relative flex items-center my-5">
-      <Image src="/images/book_a_meeting.svg" width={0} height={0} alt="book us" className="w-full" />
-      <div className="absolute ms-20">
-        <h1>build your dream <br /> room today!</h1>
-        <button className='mt-5 flex'>book a meeting
+      {/* <Image src="/images/book_a_meeting.svg"  width={0} height={0} alt="book us" className="img" /> */}
+      <div className="absolute top-10 left-10">
+        <h1 className='text-xl md:text-2xl lg:3xl text-white font-semibold uppercase'>build your dream <br /> room today!</h1>
+        <button className='mt-3 md:mt-5 flex'>book a meeting
           <Image src='/icons/arrow_top.svg' className="my-auto ms-2" width={16} height={0} alt="icon" />
         </button>
       </div>
